f1f8a3556ebe3cb03ca2c3e0100c6f4829b3e4f7
user-test123 <david.he613@gmail.com>
2020-01-13 13:45:27 -0500

    added changed files for refactor tail

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,26 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  return actual === expected;
-};
+// const assertEqual = function(actual, expected) {
+//   return actual === expected;
+// };
 
 // TEST CODE
-if (assertEqual("Lighthouse Labs", "Bootcamp")) {
-  console.log('Failed: "Lighthouse Labs" !== "Bootcamp"');
-} else if (assertEqual(1, 1)) {
-  console.log("Assertion Passed: 1 === 1");
-}
+// if (assertEqual("Lighthouse Labs", "Bootcamp")) {
+//   console.log('Failed: "Lighthouse Labs" !== "Bootcamp"');
+// } else if (assertEqual(1, 1)) {
+//   console.log("Assertion Passed: 1 === 1");
+// }
 
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1,1);
 // assertEqual(34,1);
 // assertEqual("hello", "hello");
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`\u2714 \u2714 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u2716 \u2716 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 module.exports = assertEqual;
--- tail.js
+++ tail.js
@@ -1,8 +1,9 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  return actual === expected;
+// const assertEqual = function(actual, expected) {
+//   return actual === expected;
 
-};
+// };
+const assertEqual = require('./assertEqual');
 
 const tail = function(actual) {
 
@@ -11,6 +12,15 @@ const tail = function(actual) {
 
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+
+// if (assertEqual("Lighthouse Labs", "Bootcamp")) {
+//   console.log('Failed: "Lighthouse Labs" !== "Bootcamp"');
+// } else if (assertEqual(1, 1)) {
+//   console.log("Assertion Passed: 1 === 1");
+// }
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
+
+module.exports = tail;
