d0d4f8f31d0d9410a37df4cd5cf5488efa9716e0
user-test123 <david.he613@gmail.com>
2020-01-13 17:50:22 -0500

    changed file for index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
-const eqArrays = require('./eqArrays');
-const assertEqual = require('./assertEqual');
 const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectssEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue')
+const flatten = require('./flatten');
+const letterPosition = require('./letterPosition');
+const map = require('./map');
+const middle = require('./middle');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
 
 
 module.exports = {
@@ -13,6 +27,19 @@ module.exports = {
   middle: middle,
   eqArrays: eqArrays,
   assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual
+  assertArraysEqual: assertArraysEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly:  countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPosition: letterPosition, 
+  map: map,
+  middle: middle,
+  takeUntil: takeUntil,
+  without: without
 
 };
