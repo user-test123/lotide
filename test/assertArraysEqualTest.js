c6f7be98fbbe7ceebbe318a4b90fe9ab3a38e7c7
user-test123 <david.he613@gmail.com>
2020-01-13 14:56:28 -0500

    added changed files middle.js

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,19 @@
+
+
 const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
 
 
-assertArraysEqual([1,2], [1,2]);
+
+// assertArraysEqual([1,2], [1,2]);
+
+// middle([1, 2, 3, 4, 5, 6]);
+// middle([1, 6, 8, 9, 10]);
 
 
+assertArraysEqual(middle([1]),[]) // => []
+assertArraysEqual(middle([1, 2]),[]) // => []
+assertArraysEqual(middle([1, 2, 3]), [2]) // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]) // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]) // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]) // => [3, 4]
