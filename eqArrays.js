cf753135d29523fd7c3d54a01ad735c2ccb70d6a
user-test123 <david.he613@gmail.com>
2020-01-13 18:52:59 -0500

    eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = require('./assertEqual');
+
+const eqArrays = function(firstArray, secondArray) {
+
+  for (let i = 0; i < firstArray.length; i++) {
+    //if (str[i] != str[lastIndex - i]) return false;
+      if (firstArray[i] !== secondArray[i]) {
+        return false;
+      }
+  } return true;
+
+};
+
+  //  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  //  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+  //  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+  //  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+
+// TEST CODE
+/*if (eqArray([1, 2, 3], [3, 2, 1])) {
+  console.log('false');
+} else if (eqArray([1, 2, 3], [1, 2, 3])) {
+  console.log("true");
+}
+*/
+
+// const assertEqual = function(actual, expected) {
+
+//   if(actual === expected) {
+//     console.log('true');
+//  } else {
+//    console.log('false');
+//  }
+// };
+module.exports = eqArrays;
